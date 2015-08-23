c6e2c6a2a9ac87a29cf8562d64b86ff7ec343955
piaoapiao <guodong@guodongdeMacBook-Pro.local>
2015-08-23 12:11:44 +0800

    Initial Commit

---
+++ TestAutoAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAutoAnimate
+//
+//  Created by guodong on 15/8/23.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAutoAnimate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestAutoAnimate
+//
+//  Created by guodong on 15/8/23.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
